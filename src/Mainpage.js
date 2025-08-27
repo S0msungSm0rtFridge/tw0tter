d27662495ecdf19449dab305af6c9f4ca8f1bbea
Nelson <nelson.chan@stonybrook.edu>
2025-08-27 00:03:12 -0400

    basic hompage no functiona;llity

---
+++ src/Mainpage.js
@@ -0,0 +1,72 @@
+import './Mainpage.css';
+
+function HomePage() {
+    return (
+        <div className = "homepage">
+            <div className = "left-nav-Bar">
+                {leftNavBar()}
+            </div>
+
+            <div className = "main-content">
+                {mainContent()}
+            </div>
+
+            <div className = "right-nav-bar">
+                {rightNavBar()}
+            </div>
+        </div>
+    )
+}
+
+
+function leftNavBar(){
+    return (
+        <ul className = "left-nav-bar-objects">
+            <li>Home</li>
+            <li>Explore</li>
+            <li>Notification</li>
+            <li>Messages</li>
+            <li>Bookmarks</li>
+            <li>Jobs</li>
+            <li>Communities</li>
+            <li>Premium</li>
+            <li>Verified Orgs</li>
+            <li>Profile</li>
+            <li>More</li>
+        </ul>
+    )
+}
+
+function rightNavBar(){
+    return (
+        <div className = "right-nav-bar-objects">
+            <input type = "text" placeholder = "Search Tw0tter"></input>
+            <button className = "shameless-plug">SubScribe</button>
+            <div className = "trending-section">
+                <h3>Trending For People Like You</h3>
+                <ul className = "trending-list">
+                    <li>Trending 1</li>
+                    <li>Trending 2</li>
+                    <li>Trending 3</li>
+                    <li>Trending 4</li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+function mainContent(){
+    return (
+        <div className = "main-content-items">
+            <div className = "post-creation-area">
+                <textarea placeholder = "What's happening?"></textarea>
+                {/*add biuttons to add photos, links giffs and shit */}
+                <button>Post</button>
+            </div>
+            <div className = "content-area">
+                {/* need to add post to this area*/}
+            </div>
+        </div>
+    )
+}
+export default HomePage;
