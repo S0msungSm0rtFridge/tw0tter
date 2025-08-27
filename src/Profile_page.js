02bf8b9c82a94812ee40d91ad008b269c371fba8
Mxlin05 <Mattxl1515@gmail.com>
2025-08-27 00:04:29 -0400

    profile page UI

---
+++ src/Profile_page.js
@@ -0,0 +1,24 @@
+export default function Profile_page(){
+    return (
+        <div>
+            <div>
+                <img src="https://pbs.twimg.com/profile_images/1960356490335641600/K4hYYe3T_400x400.png" alt="nono"></img>
+                <button>edit profile</button>
+            </div>
+            <div>
+                <h1>Name</h1>
+                <h5>@username</h5>
+                <h5>Joined date</h5>
+            </div>
+            <div>
+                <button>Following</button>
+                <button>Followers</button>
+            </div>
+            <div>
+                <button>Posts</button>
+                <button>Replies</button>
+                <button>Likes</button>
+            </div>
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Profile_page from './Profile_page';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Profile_page />
   </React.StrictMode>
 );
 
